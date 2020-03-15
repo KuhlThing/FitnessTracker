c083bcc80c1bb24ce8f22c19e1f5b34a1df51a36
Patrick McDowell <kuhlthing@gmail.com>
2020-03-15 01:05:58 -0400

    fixed all files from alt draft

--- models/exercise.js
+++ models/exercise.js
@@ -5,30 +5,30 @@ const ExerciseSchema = new Schema({
   type: {
     type: String,
     enum: ["cardio", "resistance"],
-    required: "Please select an exercise type",
+    required: "Please select an exercise type"
   },
   duration: {
     type: Number,
     required: "Duration is required",
-    min: 1,
+    min: 1
   },
   name: {
     type: String,
     trim: true,
     minlength: 1,
-    required: "Please enter a name",
+    required: "Please enter a name"
   },
   weight: {
     type: Number,
-    min: 0,
+    min: 0
   },
   reps: {
     type: Number,
-    min: 0,
+    min: 0
   },
   sets: {
     type: Number,
-    min: 0,
+    min: 0
   },
   distance: {
     type: Number,
